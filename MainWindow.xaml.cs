02e2ccaf613769601fad7d6603e6627e70413cad
4elmol <ne4eLoDoIMoLoVeK@yandex.ru>
2025-10-27 14:58:26 +0300

    454

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,13 +11,16 @@ using System.Windows.Shapes;
 
 namespace WPF_Demo;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+        MessageBox.Show("Hello World");
     }
 }
